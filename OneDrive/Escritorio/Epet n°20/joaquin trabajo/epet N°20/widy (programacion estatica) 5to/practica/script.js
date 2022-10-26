9d6605254b0c0c38e7928637563e04d7f76cde69
joaquin Olate <joaquinolate36@gmail.com>
2022-10-26 18:46:45 -0300

    function elemento

--- OneDrive/Escritorio/Epet n°20/joaquin trabajo/epet N°20/widy (programacion estatica) 5to/practica/script.js
+++ OneDrive/Escritorio/Epet n°20/joaquin trabajo/epet N°20/widy (programacion estatica) 5to/practica/script.js
@@ -1,1 +1,8 @@
 var capa = document.getElementById("capa");
+
+function addElemento(texto){
+    var capa = document.getElementById("capa");
+    var h1 = document.createElement("h1");
+    h1.innerHTML = texto;
+    capa.appendChild(h1);
+}
